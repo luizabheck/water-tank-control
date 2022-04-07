327ff2f633b8d9d952c426283331e1e9ef97efdd
luizabheck <luizabheck@gmail.com>
2022-04-07 12:32:33 -0300

    included plant and angle out models

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 all: client server
 
 client:
-	gcc -o client client.c -lpthread
+	gcc -o client client.c -lpthread -lm
 
 server:
-	gcc -o server server.c -lpthread
+	gcc -o server server.c -lpthread -lm
--- server.c
+++ server.c
@@ -5,16 +5,41 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <math.h>
 
 #define BUFFER_SIZE 2000
+#define MAX_FLUX_INITIAL 100
+#define ANGLE_INITIAL 50
+#define LEVEL_INITIAL 0.4
 
 int sendMsgToClient(int socket_desc, char *server_message, struct sockaddr *client_addr);
 int receiveMsgFromClient(int socket_desc, char *client_message, struct sockaddr *client_addr);
 void messageHandler(char *client_message, char *server_message);
 void *serverThreadFunction(void *arg);
 void *plantThreadFunction(void *arg);
+float clamp(float value, float max, float min);
+double outAngle(double tempo);
 
-float water_level = 0.4;
+typedef struct
+{                     /*Struct to store values concerning to the plant being simulated*/
+    double max_flux;  /*Max out flux; Can be changed via SetMax command*/
+    double angle;     /*Input Valve angle*/
+    double out_angle; /*Output Valve angle*/
+    double level;     /*Current plant level*/
+    double passed_time;
+} plant_t;
+
+// Global variables
+char *cmd;
+int value;
+
+plant_t plant = {
+    .max_flux = MAX_FLUX_INITIAL,
+    .angle = ANGLE_INITIAL,
+    .out_angle = 0,
+    .level = LEVEL_INITIAL,
+    .passed_time = 0,
+};
 
 int main(void)
 {
@@ -29,12 +54,109 @@ int main(void)
     return 0;
 }
 
+float clamp(float value, float min, float max)
+{
+    if (value >= max)
+    {
+        return max;
+    }
+    else if (value <= min)
+    {
+        return min;
+    }
+
+    return value;
+}
+
+double outAngle(double tempo)
+{
+    if (tempo <= 0)
+    {
+        return 50; /*Starting value*/
+    }
+    if (tempo < 20000)
+    {
+        return (50 + tempo / 400); /*Ramp up*/
+    }
+    if (tempo < 30000)
+    {
+        return 100;
+    }
+    if (tempo < 50000)
+    {
+        return (100 - (tempo - 30000) / 250); /*Ramp down*/
+    }
+    if (tempo < 70000)
+    {
+        return (20 + (tempo - 50000) / 1000);
+    }
+    if (tempo < 100000)
+    {
+        return (40 + 20 * cos((tempo - 70000) * 2 * M_PI / 10000));
+    }
+    return 100;
+}
+
 void *plantThreadFunction(void *arg)
 {
+    float delta = 0, influx = 0, outflux = 0;
+    int dT_ms = 10;
+
     while (1)
     {
-        printf("alguma coisa\n");
-        // do something
+        if (cmd != NULL)
+        {
+            if (strcmp(cmd, "OpenValve") == 0) /*If are equal*/
+            {
+                delta += value;
+            }
+            if (strcmp(cmd, "CloseValve") == 0)
+            {
+                delta -= value;
+            }
+            if (strcmp(cmd, "SetMax") == 0)
+            {
+                plant.max_flux = value;
+            }
+        }
+
+        printf("\nPlant delta1: %.2f\n", delta);
+
+        if (delta > 0)
+        {
+            if (delta < 0.01 * dT_ms)
+            {
+                plant.angle = clamp(plant.angle + delta, 0, 100);
+                delta = 0;
+            }
+            else
+            {
+                plant.angle = clamp(plant.angle + 0.01 * dT_ms, 0, 100);
+                delta -= 0.01 * dT_ms;
+            }
+            printf("Plant delta2: %.2f\n", delta);
+        }
+        else if (delta < 0)
+        {
+            if (delta > -0.01 * dT_ms)
+            {
+                plant.angle = clamp(plant.angle + delta, 0, 100);
+                delta = 0;
+            }
+            else
+            {
+                plant.angle = clamp(plant.angle - 0.01 * dT_ms, 0, 100);
+                delta += 0.01 * dT_ms;
+            }
+        }
+        printf("Plant angle: %.2f\n", plant.angle);
+
+        influx = 1 * sin(M_PI / 2 * plant.angle / 100);
+        plant.out_angle = outAngle(plant.passed_time);
+        outflux = (plant.max_flux / 100) * (plant.level / 1.25 + 0.2) * sin(M_PI / 2 * (plant.out_angle) / 100);
+        plant.level = clamp(plant.level + 0.00002 * dT_ms * (influx - outflux), 0, 1);
+
+        printf("Plant level: %.2f\n", plant.level);
         usleep(1000000);
     }
 }
@@ -46,7 +168,7 @@ void *serverThreadFunction(void *arg)
     int socket_desc;
 
     // Clean buffers:
-    // Seta todos os bytes para 0
+    // Set all bytes to 0
     memset(server_message, 0, sizeof(server_message));
     memset(client_message, 0, sizeof(client_message));
 
@@ -93,8 +215,7 @@ void *serverThreadFunction(void *arg)
 
 void messageHandler(char *client_message, char *server_message)
 {
-    char *cmd, value_str[5];
-    int value;
+    char value_str[10];
 
     if (strstr(client_message, "#") == NULL) // Checks if has not received any argument
     {
@@ -115,7 +236,7 @@ void messageHandler(char *client_message, char *server_message)
     else if (strcmp(client_message, "Start") == 0)
     {
         strcpy(server_message, "Start#OK!");
-        // TODO (Re-)Iniciar o simulador da Planta
+        // TODO (Re-)Iniciar o simulador da planta
     }
     else if (strcmp(client_message, "GetLevel") == 0)
     {
